59a51a9cbb1c8949027354f6ad16f9d23c309ed8
Juhee Park <58504730+Juhee-Park@users.noreply.github.com>
2023-05-15 15:29:41 +0900

    Update IMDBStudent20180971.java
    재수정

--- HW1/IMDBStudent20180971.java
+++ HW1/IMDBStudent20180971.java
@@ -1,10 +1,5 @@
 import java.io.IOException;
 import java.util.*;
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.text.ParseException;
 
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.FileSystem;
@@ -15,127 +10,55 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class UBERStudent20180971 
+public class IMDBStudent20180971
 {
 
-	public static class UBERMapper extends Mapper<Object, Text, Text, Text>
+	public static class IMDbMapper extends Mapper<Object, Text, Text, IntWritable>
 	{
-		private Text key_word = new Text();
-		private Text value_word = new Text();
+		private final static IntWritable one = new IntWritable(1);
+		private Text genre_list = new Text();
+		private Text genre = new Text();
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
 		{
-			StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+			//StringTokenizer가 작동하도록 문자 2개를 다른 하나의 문자로 바꿈
+			String value_string = value.toString().replaceAll("::", "@");
+			
+			//바꾼 문자를 기준으로 토큰 분리
+			StringTokenizer itr = new StringTokenizer(value_string, "@");
 			while (itr.hasMoreTokens()) 
 			{
-				//차량 번호
-				String base_number = itr.nextToken();
+			
+				//영화 번호
+				int index = Integer.parseInt(itr.nextToken().trim());
+				//영화 제목
+				String movie_name = itr.nextToken();				
+				//영화 장르 목록
+				genre_list.set(itr.nextToken());
+	
+				StringTokenizer genre_itr = new StringTokenizer(genre_list.toString(), "|");
 				
-				//날짜
-				String inputDate = itr.nextToken();	
-				
-				//날짜 형식에 맞게 포멧에 집어넣음
-				DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-				
-				Date date = new Date();
-				try{
-					date = df.parse(inputDate);
-				} 
-				catch (ParseException e) {
-					System.err.println("error");
-				}
-				
-				//Calendar클래스의 DAY_OF_WEEK를 통해 요일 숫자를 계산
-				Calendar cal = Calendar.getInstance();
-				cal.setTime(date);
-				
-				String week_num = Integer.toString(cal.get(Calendar.DAY_OF_WEEK) - 1);
-				
-				//일요일을 맨 뒤로
-				if (week_num.equals("0")) {
-					week_num = "7";
-				}
-											
-				String active_vehicles = itr.nextToken().trim();
-				String trips = itr.nextToken().trim();				
-				
-				//key = <차량 번호,요일(숫자)>, value = <trips,active vehicles>
-				//요일을 숫자로 넘김으로써 월요일부터 순서대로 정렬가능 			
-				key_word.set(base_number + "," + week_num);
-				value_word.set(trips + "," + active_vehicles);
-				
-				if(base_number.equals("B02512") && week_num.equals("4"))
-				{
-					System.out.println(date);
-				}
-				context.write(key_word, value_word);				
+				//목록을 나눈 뒤 1과 함께 매핑
+				while (genre_itr.hasMoreTokens()) {
+					genre.set(genre_itr.nextToken());
+					context.write(genre, one);
+				}				
 			}
 		}
 	}
 
-	public static class UBERReducer extends Reducer<Text, Text, Text, Text> 
+	public static class IMDbReducer extends Reducer<Text, IntWritable, Text, IntWritable> 
 	{
-		//
-		private String [] weeks = {"", "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
-		private Text new_key = new Text();
-		private Text result = new Text();
-
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException 
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
 		{
-			int sum_trips = 0;
-			int sum_active_vehicles = 0;
-			
-			//key를 수정
-			StringTokenizer itr_key = new StringTokenizer(key.toString(), ",");
-			while (itr_key.hasMoreTokens())
+			// value의 총합 계산
+			int sum = 0;
+			for (IntWritable val : values) 
 			{
-				String base_number = itr_key.nextToken();
-				int weeks_num = Integer.parseInt(itr_key.nextToken().trim());
-			
-				
-				//요일 번호를 요일 문자로 바꿔줌
-				String week = weeks[weeks_num];
-				if(base_number.equals("B02512") && weeks_num == 4)
-				{
-					System.out.println(weeks[weeks_num]);
-				}
-				new_key.set(base_number + "," + week);
+				sum += val.get();
 			}
-			
-			for (Text val : values) 
-			{
-				//Text 형식으로 넘겨받은 value 값을 trips과 active vehicles로 나눈 뒤
-				//총 합을 구해줌
-				StringTokenizer itr = new StringTokenizer(val.toString(), ",");
-				while (itr.hasMoreTokens())
-				{
-					
-					int trips = Integer.parseInt(itr.nextToken().trim());
-					int active_vehicles = Integer.parseInt(itr.nextToken().trim());
-					
-					if(key.toString().equals("B02512,4"))
-					{
-						System.out.print(trips);
-						System.out.print("  ");
-						System.out.println(active_vehicles);
-					}
-					
-					sum_trips += trips;
-					sum_active_vehicles += active_vehicles;			
-				}				
-			}
-			
-			String sum = Integer.toString(sum_trips) + "," 
-				+ Integer.toString(sum_active_vehicles);
-			
-								
-					if(key.toString().equals("B02512,4"))
-					{
-						System.out.println(sum);
-					}
-				
 			result.set(sum);
-			context.write(new_key, result);
+			context.write(key, result);
 		}
 	}
 
@@ -145,17 +68,18 @@ public class UBERStudent20180971
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 		if (otherArgs.length != 2) 
 		{
-			System.err.println("Usage: UBER <in> <out>");
+			System.err.println("Usage: IMDb <in> <out>");
 			System.exit(2);
 		}
 		Job job = new Job(conf, "IMDb");	
-		job.setJarByClass(UBERStudent20180971.class);
+		job.setJarByClass(IMDBStudent20180971.class);
 		
-		job.setMapperClass(UBERMapper.class);
-		job.setReducerClass(UBERReducer.class);
+		job.setMapperClass(IMDbMapper.class);
+		job.setCombinerClass(IMDbReducer.class);
+		job.setReducerClass(IMDbReducer.class);
 		
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 		
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
